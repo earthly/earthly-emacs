a9432f54187a49eacc1d0df52c57edc1b72067f4
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2021-08-28 08:47:27 +0700

    Support .earth file extension

--- earthly-mode.el
+++ earthly-mode.el
@@ -86,7 +86,9 @@
   (setq-local comment-end "")
   (setq-local font-lock-defaults '(earthly-build-font-lock-keywords)))
 
-(add-to-list 'auto-mode-alist '("Earthfile" . earthly-mode))
+(progn
+  (add-to-list 'auto-mode-alist '("Earthfile" . earthly-mode))
+  (add-to-list 'auto-mode-alist '("\\.earth\\'" . earthly-mode)))
 
 (provide 'earthly-mode)
 
