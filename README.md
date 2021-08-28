84c3a2548d233936c2174c1f39f73eff0a0e626a
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2021-08-28 11:49:10 +0700

    Initiate earthly-mode (#1)
    * Initial commit
    
    * Add basic features
    
    * Support basic keyword highlighting base on https://docs.earthly.dev/docs/earthfile
      documentation.
    * Highlight comment code.
    * Toggle comment support.
    
    * Add variable highlight support
    
    * Rename earthly.el to earthly-mode.el
    
    * Fix Earthfile not load earthly-mode automatically
    
    * Use `[[:space::]]` instead of `\\s`
    
    * Fix incorrect `WITH DOCKER` syntax
    
    * Remove FOR IN out of keywords
    
    `FOR IN` needs to implements on it own custom keywords because `IN`
    can be match with variable.
    
    * Fix keyword highlight incorrectly
    
    Keywords should beginning at line start. And fix `"[[:space]]"` character
    not work because it got escaped by `rx` function.
    
    * Add FOR command back
    
    The `IN` is not support at the moment.
    
    * Format code
    
    * Add header comment and lexical-binding
    
    * Support FOR..IN
    
    * Add ELSE and ELSE IF
    
    * Update README.md
    
    * Add code header
    
    * Update README.md
    
    * Update README.md
    
    * Support SAVE ARTIFACT .. AS LOCAL ..
    
    * Fix incorrect add extension
    
    In example, it'll ignore `Earthfile` if we just put only `.earth` extension.
    
    * Add screenshot
    
    * Fix screenshot not load
    
    * Update screenshot
    
    * Add .gitignore
    
    * Fix issues from suggested by package-lint.el
    
    * Extract regexp in earthly-build-font-lock-keywords to defconst
    
    * Make checkdoc pass
    
    * Remove eval out of keywords
    
    * Fix keyword highlight on target name
    
    * Refactor auto-mode-alist
    
    * Support .earth file extension
    
    * Update doc comment
    
    * Update README.md
    
    Add contributor and remove highlight `.earth` file setup section.
    
    * Remove .DS_Store from gitignore

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # earthly-mode
+
+Major mode for editing Earthly Earthfile (https://docs.earthly.dev/docs/earthfile) developed
+by @wingyplus, @TheCoreMan and @duck1123
+
+## Current features
+
+* Syntax highlighting support.
+* Toggle comment.
+
+## Installation Notes
+
+### use-package
+
+If you're using `use-package`, you can do:
+
+```elisp
+(use-package
+  :ensure t)
+```
+
+### Manual
+
+Add this major mode to `load-path` using `add-to-list`:
+
+```elisp
+(add-to-list 'load-path "path/to/major/mode")
+```
+
+## Screenshot
+
+![Earthfile syntax](Screenshot.png)
---
+++ earthly-mode.el
@@ -0,0 +1,95 @@
+;;; earthly-mode.el --- Major mode for editing Earthly file -*- lexical-binding: t -*-
+
+;; Author: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
+;; URL: https://github.com/wingyplus/earthly-mode
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "26"))
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; This package provides syntax highlight and toggle comment support
+;; for Earthly files.
+
+;;; Code:
+
+(defconst earthly-keywords-regexp
+  (rx line-start
+      (* space)
+      (or "FROM"
+	  "RUN"
+	  "COPY"
+	  "ARG"
+	  "SAVE ARTIFACT"
+	  "SAVE IMAGE"
+	  "BUILD"
+	  "VERSION"
+	  "GIT CLONE"
+	  "CMD"
+	  "LABEL"
+	  "EXPOSE"
+	  "ENV"
+	  "ENTRYPOINT"
+	  "VOLUME"
+	  "USER"
+	  "WORKDIR"
+	  "HEALTHCHECK NONE"
+	  "HEALTHCHECK CMD"
+	  "FROM DOCKERFILE"
+	  "WITH DOCKER"
+	  "IF"
+	  "ELSE"
+	  "ELSE IF"
+	  "FOR"
+	  "END"
+	  "LOCALLY"
+	  "COMMAND"
+	  "DO"
+	  "IMPORT")
+      (or (+ space) line-end))
+  "All Earthly keywords.")
+
+(defconst earthly-variable-regexp
+  (rx (sequence "$" (? "{") (+ (in (?A . ?Z) (?a . ?z) (?0 . ?9) ?- ?_)) (? "}")))
+  "Regular Expression for Earthly variable.")
+
+(defconst earthly-for-keyword-regexp
+  (rx line-start (* space) (group "FOR") word-boundary (*? (regex ".")) word-boundary (group "IN") word-boundary)
+  "Regular Expression for Earthly FOR .. IN syntax.")
+
+(defconst earthly-save-artifact-keyword-regexp
+  (rx line-start (* space) (group "SAVE ARTIFACT") word-boundary (*? (regex ".")) word-boundary (group "AS LOCAL") word-boundary)
+  "Regular Expression for Earthly SAVE ARTIFACT .. AS LOCAL syntax.")
+
+(defun earthly-build-font-lock-keywords ()
+  "Build font lock for Earthly syntax."
+  (list
+    `(,earthly-keywords-regexp . font-lock-keyword-face)
+    `(,earthly-variable-regexp . font-lock-variable-name-face)
+    `(,earthly-for-keyword-regexp (1 font-lock-keyword-face) (2 font-lock-keyword-face))
+    `(,earthly-save-artifact-keyword-regexp (1 font-lock-keyword-face) (2 font-lock-keyword-face))))
+
+(defvar earthly-syntax-table
+  (let ((syntax-table (make-syntax-table)))
+    (modify-syntax-entry ?\# "<" syntax-table)
+    (modify-syntax-entry ?\n ">" syntax-table)
+    (modify-syntax-entry ?\" "\"" syntax-table)
+    (modify-syntax-entry ?\' "\"" syntax-table)
+    syntax-table)
+  "Syntax table for `earthly-mode'.")
+
+(define-derived-mode earthly-mode prog-mode "Earthly"
+  "A major mode for editing Earthly file."
+  :syntax-table earthly-syntax-table
+  (setq-local comment-start "#")
+  (setq-local comment-end "")
+  (setq-local font-lock-defaults '(earthly-build-font-lock-keywords)))
+
+(progn
+  (add-to-list 'auto-mode-alist '("Earthfile" . earthly-mode))
+  (add-to-list 'auto-mode-alist '("\\.earth\\'" . earthly-mode)))
+
+(provide 'earthly-mode)
+
+;;; earthly-mode.el ends here
